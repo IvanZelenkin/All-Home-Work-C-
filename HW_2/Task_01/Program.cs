35fa41489a3a245084ba2dc8a8e7e6364bc82c48
IvanZelenkin <vanya-200@yandex.ru>
2023-02-12 15:03:42 +0300

    Поиск второй цифры в трехзачном числе

---
+++ HW_2/Task_01/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int boba = n/10;
+System.Console.WriteLine(boba); //Делим на 10. Получаем двузначное ЦЕЛОЕ число
+int bob = boba%10;
+System.Console.WriteLine(bob); //Берем двузначное чило и получаем остаток
