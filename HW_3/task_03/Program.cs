4cbc03334fe6181f015626b5382be0e5394b1dcf
IvanZelenkin <vanya-200@yandex.ru>
2023-02-13 16:38:40 +0300

    Поиск куба от 1 до N

--- HW_3/task_03/Program.cs
+++ HW_3/task_03/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int n = InputNumber("Введите число N");
+
+
+for (int count = 1; count <= n; count++)
+{
+    System.Console.Write(Math.Pow(count, 3) + ", ");
+}
