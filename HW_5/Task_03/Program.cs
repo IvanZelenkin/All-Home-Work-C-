96a40c48c4ea115520d3adcf86f6880cf80bb235
IvanZelenkin <vanya-200@yandex.ru>
2023-02-22 12:28:11 +0300

    double diff

--- HW_5/Task_03/Program.cs
+++ HW_5/Task_03/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+double FindDifference(double[] array)
+{
+    double min = array[0];
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    double diff = max - min;
+    return diff;
+}
+
+void PrintArray(double[] array)
+{
+    System.Console.Write("[" + string.Join(". ", array) + "]");
+}
+
+double[] myarr = { 3.2, 7.4, 22.8, 2.3, 78.7 };
+double diff = FindDifference(myarr);
+PrintArray(myarr);
+System.Console.Write(" -> " + diff);
