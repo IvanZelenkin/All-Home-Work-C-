e4a379879b956560e79b91313f606c0e23d15fc2
IvanZelenkin <vanya-200@yandex.ru>
2023-03-12 16:51:43 +0300

    task1 complete

--- HW_7/task_1/Program.cs
+++ HW_7/task_1/Program.cs
@@ -3,3 +3,30 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+double[,] GenerateMatrix(int rows, int cols)
+{
+     double [,] matrix = new double [rows,cols];
+
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            matrix [i, j] = new Random().NextDouble();
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double [,] arr)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+    {
+        for (int j=0; j<arr.GetLength(1); j++)
+        {
+           Console.Write($"{Math.Round(arr[i,j],2)} \t ");
+        }
+        System.Console.WriteLine();
+    }
+}
+var array = GenerateMatrix(4,4); 
+PrintMatrix(array);
