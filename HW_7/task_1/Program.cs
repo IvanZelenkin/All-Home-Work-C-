50c9c9828706720919c69b072efbf9b23b5f3d28
IvanZelenkin <vanya-200@yandex.ru>
2023-03-12 17:46:54 +0300

    task2 complete

--- HW_7/task_1/Program.cs
+++ HW_7/task_1/Program.cs
@@ -3,30 +3,39 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
-
-double[,] GenerateMatrix(int rows, int cols)
+double ReadInt(string text)
 {
-     double [,] matrix = new double [rows,cols];
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-    for (int i=0; i<matrix.GetLength(0); i++)
+double[,] GenerateMatrix(double rows, double cols)
+{
+    Random rand = new Random();
+    double[,] matrix = new double[(int)rows, (int)cols];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j=0; j<matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix [i, j] = new Random().NextDouble();
+            matrix[i, j] = Math.Round(rand.NextDouble(), 3);
         }
     }
     return matrix;
 }
-void PrintMatrix(double [,] arr)
+void PrintMatrix(double[,] arr)
 {
-    for (int i=0; i<arr.GetLength(0); i++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j=0; j<arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-           Console.Write($"{Math.Round(arr[i,j],2)} \t ");
+            Console.Write($"{Math.Round(arr[i, j], 2)} \t ");
         }
         System.Console.WriteLine();
     }
 }
-var array = GenerateMatrix(4,4); 
+var rows = ReadInt("Введите количество строк матрицы");
+var cols = ReadInt("Введите количество столбцов матрицы");
+
+var array = GenerateMatrix(rows, cols);
 PrintMatrix(array);
--- HW_7/task_2/Program.cs
+++ HW_7/task_2/Program.cs
@@ -5,3 +5,75 @@
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int PozElements(int[,] matrix, int pozI, int pozJ)
+{
+    int element = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (pozI < matrix.GetLength(0) && pozJ < matrix.GetLength(1))
+            {
+                element = matrix[pozI, pozJ];
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"Заданной позиции в массиве не существует:[{pozI};{pozJ}]");
+            }
+        }
+    }
+    return element;
+}
+
+void PrintPozElements(int[,] matrix)
+{
+    int pozI = new int();
+    int pozJ = new int();
+    Console.Write($" На заданной позиции находится элемент: {matrix[pozI, pozJ]}");
+}
+
+int rows = ReadInt("Введите количество строк матрицы");
+int cols = ReadInt("Введите количество столбцов матрицы");
+int pozI = ReadInt("Введите позицию элемента в строке матрицы");
+int pozJ = ReadInt("Введите позицию элемента в столбце матрицы");
+
+var myMatrix = GenerateMatrix(rows, cols);
+PrintMatrix(myMatrix);
+
+System.Console.WriteLine();
+
+var myPoz = PozElements(myMatrix, pozI, pozJ);
+PrintPozElements(myMatrix);
